8b6b7a3a3131df337150eb6882ba5bb264cedfe6
Dardev12 <73501755+Dardev12@users.noreply.github.com>
2021-11-07 14:22:31 -0500

    Init MainScriptAssistant

---
+++ MainScriptAssistant.ps1
@@ -0,0 +1,65 @@
+function MonteBot{
+    param(
+        $sayTheLine,
+        $rate
+    )
+
+    Add-Type -AssemblyName System.speech
+    $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $speak | Get-Member
+    Foreach ($voice in $speak.GetInstalledVoices()){
+    $Voice.VoiceInfo | Select-Object Gender, Name, Culture, Description
+    }
+    $speak.Rate = $rate
+    $speak.Speak($sayTheLine)
+
+}
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+$Form = New-Object System.Windows.Forms.Form
+$Form.ClientSize = '300,300'
+$Form.Text = "MonteBot Interface"
+
+$BoutonEducation = New-Object System.Windows.Forms.Button
+$BoutonEducation.Location = New-Object System.Drawing.Point(100,100)
+$BoutonEducation.Width = 80
+$BoutonEducation.Height = 40
+$BoutonEducation.Text = "Session Etude"
+$BoutonEducation.Add_Click({
+    $Label.Text = "Passons au choses sérieuse !!!"
+    MonteBot -sayTheLine "Passons au choses sérieuse !!!" -rate 0
+})
+
+$BoutonGaming = New-Object System.Windows.Forms.Button
+$BoutonGaming.Location = New-Object System.Drawing.Point(100,150)
+$BoutonGaming.Width = 80
+$BoutonGaming.Height = 40
+$BoutonGaming.Text = "Session Gaming"
+$BoutonGaming.Add_Click({
+    $Label.Text = "Gaming Lets go"
+    MonteBot -sayTheLine "Gaming Lets go" -rate 5
+})
+
+$BoutonClose = New-Object System.Windows.Forms.Button
+$BoutonClose.Location = New-Object System.Drawing.Point(100,200)
+$BoutonClose.Width = 80
+$BoutonClose.Height = 40
+$BoutonClose.Text = "Débrancher MonteBot"
+$BoutonClose.Add_Click({
+    $Label.Text = "-_- Goodbye "
+    MonteBot -sayTheLine "Good bye " -rate -10
+})
+
+$Label = New-Object System.Windows.Forms.Label
+$Label.Location = New-Object System.Drawing.Point(100,50)
+$Label.Size = '100,100'
+$Label.Text = "bonjour"
+
+$Title = New-Object System.Windows.Forms.Label
+$Title.Font = [System.Drawing.Font]::new("Microsoft Sans Serif", 12, [System.Drawing.FontStyle]::Bold)
+$Title.Location = New-Object System.Drawing.Point(100,30)
+$Title.Text = "MonteBot"
+
+$Form.controls.AddRange(@($BoutonEducation,$Label,$BoutonGaming,$Title,$BoutonClose))
+$Form.ShowDialog()
