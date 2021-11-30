749efc952f5c220a72c5a03e592236baccca1908
Dardev12 <73501755+Dardev12@users.noreply.github.com>
2021-11-30 15:01:37 -0500

    Update Profil Gaming

--- ScriptProfilGaming.ps1
+++ ScriptProfilGaming.ps1
@@ -10,3 +10,6 @@ ForEach($s in $site)
 Start-Sleep -Seconds 2
 $shell = New-Object -ComObject "Shell.Application"
 $shell.minimizeall()
+
+Start-Sleep -Seconds 2
+.\SetWallPaper.ps1 "C:\Users\533\Documents\GitHub\AssitantPowershell\gaming.jpg" "Stretch"
